b06f90f6a25f9d9deb2e91d64e9480b6b31b1eb2
asfawe <aronia5137>
2022-02-27 21:43:38 +0900

    first commit

---
+++ variable.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<body>
+    <h1>Variable</h1>
+    <?php
+    $name = 'William';
+    echo 'Lorem ipsum dolor sit'.$name.'amet consectetur adipisicing elit. '.$name.' Ipsam ipsum ad placeat '.$name.' maiores doloremque William consequuntur, quos commodi William quo rem voluptatem mollitia reiciendis quod sint impedit excepturi assumenda provident repellendus adipisci.'
+
+    ?>
+</body>
+</html>
